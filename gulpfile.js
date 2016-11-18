6d18d82449b46f7c7e1268a12341d2a2c82fedad
plsr <christianpoplawski@gmail.com>
2016-11-18 16:53:48 +0100

    Added autoprefixer to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function(){
   return gulp.src('src/styles/**/*.sass')
     .pipe(sass()) // Using gulp-sass
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3'],
+            cascade: false
+    }))
     .pipe(gulp.dest('src'))
 });
 
