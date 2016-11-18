4b467988563ebd89b8a31943437cfa8d91006509
plsr <christianpoplawski@gmail.com>
2016-11-18 17:20:08 +0100

    Added foundation css to project

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function(){
   return gulp.src('src/styles/**/*.sass')
-    .pipe(sass()) // Using gulp-sass
+    .pipe(sass({
+      includePaths: 'node_modules/foundation-sites/scss'
+    })) // Using gulp-sass
     .pipe(autoprefixer({
             browsers: ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3'],
             cascade: false
